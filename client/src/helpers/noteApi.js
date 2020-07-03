edce2c37f46ab421cb82caab00a6744c95669bec
majkon8 <48221693+majkon8@users.noreply.github.com>
2020-07-03 20:06:10 +0200

    Fix deployment to Heroku

--- client/src/helpers/noteApi.js
+++ client/src/helpers/noteApi.js
@@ -1,12 +1,13 @@
-import { notesUrl } from "./url";
 import * as api from "./api";
+
+export const notesUrl = (id) => (id ? `/notes/${id}` : `/notes`);
 
 export const getAll = () => api.get(notesUrl());
 
-export const get = id => api.get(notesUrl(id));
+export const get = (id) => api.get(notesUrl(id));
 
-export const create = body => api.post(notesUrl(), body);
+export const create = (body) => api.post(notesUrl(), body);
 
 export const update = (id, body) => api.put(notesUrl(id), body);
 
-export const destroy = id => api.destroy(notesUrl(id));
+export const destroy = (id) => api.destroy(notesUrl(id));
--- client/src/helpers/url.js
+++
@@ -1,4 +0,0 @@
-const serverUrl = "http://localhost:3000";
-
-export const notesUrl = id =>
-  id ? `/notes/${id}` : `/notes`;
