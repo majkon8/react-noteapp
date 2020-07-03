3c03a4a047a13362a9c5e15108e68b012cb27f20
majkon8 <48221693+majkon8@users.noreply.github.com>
2020-07-03 19:52:16 +0200

    Fix deployment to Heroku

--- server.js
+++ server.js
@@ -33,12 +33,8 @@ mongoose
     process.exit();
   });
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to NoteApp" });
-});
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client", "build")))
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
